f5da387b20eb640c44636b3acb7b8e4c0f626748
soh3ee <sohee@machie.local>
2024-01-10 11:43:22 -0800

    yuh

---
+++ raven/extra.go
@@ -0,0 +1,22 @@
+1. clone
+
+2. install bee cli tool
+```
+go install github.com/beego/bee/v2@latest
+```
+
+3. change directory into tool
+```
+cd raven/
+```
+
+4. init the module and download libraries
+```
+go mod init raven
+go mod tidy
+```
+
+5. run
+```
+bee run
+```
